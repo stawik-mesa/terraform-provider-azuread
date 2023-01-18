d9ea9dfe153ae494d1f82af1c0eb6f96ac91b0be
stawiklu <stawiklu@mediamarkt.at>
2023-01-18 14:22:03 +0100

    Fix go mod

--- vendor/github.com/manicminer/hamilton/msgraph/models.go
+++ vendor/github.com/manicminer/hamilton/msgraph/models.go
@@ -620,7 +620,6 @@ type ConditionalAccessPolicy struct {
 type ConditionalAccessSessionControls struct {
 	ApplicationEnforcedRestrictions *ApplicationEnforcedRestrictionsSessionControl `json:"applicationEnforcedRestrictions,omitempty"`
 	CloudAppSecurity                *CloudAppSecurityControl                       `json:"cloudAppSecurity,omitempty"`
-	DisableResilienceDefaults       *DisableResilienceDefaultsSessionControl       `json:"disableResilienceDefaults,omitempty"`
 	PersistentBrowser               *PersistentBrowserSessionControl               `json:"persistentBrowser,omitempty"`
 	SignInFrequency                 *SignInFrequencySessionControl                 `json:"signInFrequency,omitempty"`
 }
@@ -688,10 +687,6 @@ type DelegatedPermissionGrant struct {
 	PrincipalId *string                              `json:"principalId,omitempty"`
 	ResourceId  *string                              `json:"resourceId,omitempty"`
 	Scopes      *[]string                            `json:"-"`
-}
-
-type DisableResilienceDefaultsSessionControl struct {
-	IsEnabled *bool `json:"isEnabled,omitempty"`
 }
 
 func (d DelegatedPermissionGrant) MarshalJSON() ([]byte, error) {
